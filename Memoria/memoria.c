8b96acaf917ae1afd291693e5940cfecc7500d65
janicead <janicefabulus@live.com.ar>
2019-06-14 19:54:28 -0300

    Gossip entre memorias funcionando correctamente

--- Memoria/memoria.c
+++ Memoria/memoria.c
@@ -2,27 +2,26 @@
 
 int main(void) {
 
-	pthread_t multiplexacionMemoria, clienteMemoria;
-
+	pthread_t multiplexacionMemoria, clienteMemoria, gossip;
+	//crearConsolaMemoria();
 	configurarLoggerMemoria();
 	configMemoria =leerConfigMemoria();
 	mostrarDatosArchivoConfig();
-	tablaDeSegmentos = list_create();
-	definirTamanioMemoriaPrincipal(1);
-	crearConsolaMemoria();
-	/*iniciarEscuchaMemoria();
+	iniciarEscuchaMemoria();
 	t_list * listaMemoriasQueMeConecte=  list_create();
+	tablaDeGossip = list_create();
+	agregarATablaDeGossip(configMemoria.puertoDeEscucha,configMemoria.ipDeEscucha, configMemoria.numeroDeMemoria);
 	log_info(loggerMemoria, "Esperando Conexiones");
+	pthread_create(&gossip, NULL, realizarGossip, NULL);
 	pthread_create(&multiplexacionMemoria, NULL, realizarMultiplexacion, NULL);
 	pthread_create(&clienteMemoria, NULL, hacermeClienteDeMisServers, NULL);
+	pthread_join(gossip, NULL);
 	pthread_join(multiplexacionMemoria, NULL);
 	pthread_join(clienteMemoria, NULL);
+	pthread_join(gossip, NULL);
+	pthread_exit(gossip);
 	pthread_exit(multiplexacionMemoria);
 	pthread_exit(clienteMemoria);
-*/
-	borrarElementos();
-	list_clean(tablaDeSegmentos);
-	list_destroy(tablaDeSegmentos);
 
 	return 0;
 }
