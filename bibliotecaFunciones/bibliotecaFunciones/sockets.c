810e0483f4e978a44cdd51cc6ffe94f231eb3d72
janicead <janicefabulus@live.com.ar>
2019-07-15 02:24:19 -0300

    Revert "100 COMMITSgit add --all!  cierro issue de gossip, closes #30"
    This reverts commit fa82f6d9a0ed72d443c6a64437e55e925e406c29.

--- bibliotecaFunciones/bibliotecaFunciones/sockets.c
+++ bibliotecaFunciones/bibliotecaFunciones/sockets.c
@@ -1,5 +1,29 @@
 #include "sockets.h"
 
+/*int conectarAlServidorEric(char * ip, char * port) {
+  struct addrinfo hints;
+  struct addrinfo *server_info;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;    // Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+  hints.ai_socktype = SOCK_STREAM;  // Indica que usaremos el protocolo TCP
+
+  getaddrinfo(ip, port, &hints, &server_info);  // Carga en server_info los datos de la conexion
+
+  // 2. Creemos el socket con el nombre "server_socket" usando la "server_info" que creamos anteriormente
+  int server_socket = socket( server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol );
+
+  int retorno = connect(server_socket,server_info->ai_addr,server_info->ai_addrlen);
+
+  freeaddrinfo(server_info);  // No lo necesitamos mas
+
+  if (retorno==-1)
+  {
+	  puts("Error al conectar");
+	  exit(0);
+  }
+  return server_socket;
+} */
 int crearServidor(){
 	int servidor,aceptar = 1;
 	if((servidor= socket(AF_INET, SOCK_STREAM, 0))==-1){
